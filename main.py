b60b24623a7fd00538e004b6a6dbafca3a8baed2
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-11-22 11:40:28 +0900

    assign value of command in window cmd for color to variable and adding while loop for looping this program

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import os
 import random
+
+# the command of color in window cmd
+green = "color a"
+red = "color 4"
+white = "color 7"
+ligth_red = "color c"
+
+program_running = True
+while program_running:
+    pass
