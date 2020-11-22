c427f9fdb2bf425ee8e308a96fb229ef9a75bd12
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-11-22 12:06:37 +0900

    Adding logic of winner or loser for this program

--- main.py
+++ main.py
@@ -31,5 +31,35 @@ pili sala satuh:
         if player.lower() == "keluar" or player.lower() == "q":
             program_running = False
 
+        # logic to winner or loser
+        if computer == "batu" and player.lower() == "kertas":
+            print("kamu menan!")
+            print("===========")
+            print('"komputer": memilih batu\n"kamu": memilih kertas')
+        elif computer == "guntin" and player.lower() == "batu":
+            print("kamu menan!")
+            print("===========")
+            print('"komputer": memilih guntin\n"kamu": memilih batu')
+        elif computer == "kertas" and player.lower() == "guntin":
+            print("kamu menan!")
+            print("===========")
+            print('"komputer": memilih kertas\n"kamu": memilih guntin')
+        elif computer == "kertas" and player.lower() == "batu":
+            print("kamu kalah!")
+            print("===========")
+            print('"komputer": memilih kertas\n"kamu": memilih batu')
+        elif computer == "guntin" and player.lower() == "kertas":
+            print("kamu kalah!")
+            print("===========")
+            print('"komputer": memilih guntin\n"kamu": memilih kertas')
+        elif computer == "batu" and player.lower() == "guntin":
+            print("kamu kalah!")
+            print("===========")
+            print('"komputer": memilih batu\n"kamu": memilih guntin')
+        elif computer == player.lower():
+            print("pilihan anda sama dengan komputer!")
+        else:
+            print("pilihan tidak ditemukan? coba lagi!")
+
     except Exception as error:
         print(error)
