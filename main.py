efe3a3e9505630f994b9adcb7860e1135fe820ff
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-11-22 11:53:17 +0900

    Adding indexing keluar or q to user quit the program

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ green = "color a"
 red = "color 4"
 white = "color 7"
 ligth_red = "color c"
+color = white
 
 program_running = True
 while program_running:
@@ -20,11 +21,15 @@ pili sala satuh:
 * batu
 * kertas
 *
-* keluar (q)
+* keluar atauh q
 """) 
 
         computer = random.choice(["batu","guntin","kertas"])
         player = str(input("ketik disini: "))
-        break
+        
+        # if user want to close the program
+        if player.lower() == "keluar" or player.lower() == "q":
+            program_running = False
+
     except Exception as error:
         print(error)
